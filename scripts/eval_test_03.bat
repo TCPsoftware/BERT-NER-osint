8732fb56f3cca9afe203319a54e5e11f18287f66
tcpsoftware <1691957831@qq.com>
2023-04-23 22:20:15 +0800

    batch

---
+++ scripts/eval_test_03.bat
@@ -0,0 +1,23 @@
+
+set TASK_NAME=osint
+set BERT_BASE_DIR=%~dp0/../
+
+python %BERT_BASE_DIR%/run_ner_crf.py ^
+  --model_type=bert ^
+  --model_name_or_path=%BERT_BASE_DIR%/prev_trained_model/chinese_roberta_wwm_ext_pytorch ^
+  --task_name=%TASK_NAME% ^
+  --do_eval ^
+  --do_lower_case ^
+  --data_dir=%BERT_BASE_DIR%/datasets/APT-NER-eval_test/ ^
+  --train_max_seq_length=128 ^
+  --eval_max_seq_length=512 ^
+  --per_gpu_train_batch_size=24 ^
+  --per_gpu_eval_batch_size=24 ^
+  --learning_rate=3e-5 ^
+  --crf_learning_rate=1e-3 ^
+  --num_train_epochs=5.0 ^
+  --logging_steps=-1 ^
+  --save_steps=-1 ^
+  --output_dir=%BERT_BASE_DIR%/outputs/%TASK_NAME%_output_03_5epoch/ ^
+  --overwrite_output_dir ^
+  --seed=42
